a9cac30fc729eee5a88be2bd17b1d1cec42bf36d
Krista Kourtessis <krista.kourtessis@telus.com>
2022-11-23 09:08:19 -0800

    add snake game local host version

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",// IP address here,
+    port: 50541// PORT number here,
+  });
+  
+    conn.on("connect", () => {
+      console.log("Welcome to the game") // code that does something when the connection is first established
+    });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+   
+};
+
+console.log("Connecting ...");
+connect();
