f15c62134e054e00a36614231514729cb17dff52
Krista Kourtessis <krista.kourtessis@telus.com>
2022-11-23 13:57:49 -0800

    module input clean up

---
+++ input.js
@@ -0,0 +1,19 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.on("data", handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // your code here register it as the "data" callback handler for stdin.
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.export = {
+  setupInput
+}
--- play.js
+++ play.js
@@ -1,18 +1,26 @@
-const net = require("net");
+// const net = require("net");
 const {connect} = require("./client");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.on("data", handleUserInput);
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.on("data", handleUserInput);
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   return stdin;
+// };
 
-const handleUserInput = function (key) {
-  // your code here register it as the "data" callback handler for stdin.
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+// const handleUserInput = function (key) {
+//   // your code here register it as the "data" callback handler for stdin.
+//   if (key === '\u0003') {
+//     process.exit();
+//   }
+// };
+
+const { setupInput } = require("./input");
+
+console.log("Connecting ...");
+connect();
+
+setupInput();
+
