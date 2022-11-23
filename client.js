31292c78d2afa4d98d7ade030654e95ab062367a
Krista Kourtessis <krista.kourtessis@telus.com>
2022-11-23 12:37:59 -0800

    update code for data portion

--- client.js
+++ client.js
@@ -1,13 +1,17 @@
 const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
-    host: "localhost",// IP address here,
+    host: '165.227.47.243',// IP address here,
     port: 50541// PORT number here,
   });
   
     conn.on("connect", () => {
       console.log("Welcome to the game") // code that does something when the connection is first established
     });
+    conn.on("data", (data) => {
+      console.log(data) 
+    });
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
